eaf8c253f437f6e54a790c98eac232dafa849c7f
joranmulderij <66296620+joranmulderij@users.noreply.github.com>
2021-12-31 13:34:07 +0500

    Update circular_countdown_timer.dart

--- lib/circular_countdown_timer.dart
+++ lib/circular_countdown_timer.dart
@@ -178,7 +178,7 @@ class CircularCountDownTimerState extends State<CircularCountDownTimer>
     }
     // For s format
     else if (widget.textFormat == CountdownTextFormat.S) {
-      return '${(duration.inSeconds)}';
+      return '${(duration.inSeconds + 1)}';
     } else {
       // Default format
       return _defaultFormat(duration);
